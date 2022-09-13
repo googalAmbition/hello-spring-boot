fee248420c027d7c065a7befab62c4f113f7173f
taylor <13356896924@163.com>
2022-09-13 22:31:22 +0800

    oauth server

---
+++ 11-spring-security/oauth2-auth-server/src/main/java/com/tcoding/demo/oauth2/authserver/Oauth2AuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.tcoding.demo.oauth2.authserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2AuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2AuthServerApplication.class, args);
+    }
+
+}
