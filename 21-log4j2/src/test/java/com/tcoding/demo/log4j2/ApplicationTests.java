a42bc0ef84f6ae4a3c5d037da19d20cd3d55d0a9
taylor <13356896924@163.com>
2022-09-15 21:26:38 +0800

    log4j2

---
+++ 21-log4j2/src/test/java/com/tcoding/demo/log4j2/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcoding.demo.log4j2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
